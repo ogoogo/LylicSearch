477693c03623e8f9d6ceedc73b08848c63656310
ogoogo <ogosae.1018173@docomo.ne.jp>
2021-09-08 21:34:39 +0900

    feat:最初のpush

--- LylicSearch/ViewController.swift
+++ LylicSearch/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    var nameArray=[String?]()
+    
+    
+    
+    
+    @IBOutlet var tableView:UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource=self
+        
+        firstGetData()
+        
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text=nameArray[indexPath.row]
+        return cell!
+    }
+    
+    func firstGetData(){
+        for i in (1...9){
+            AF.request("https://swapi.dev/api/people/?page=\(i)",method: .get)
+                .responseJSON{response in
+                    switch response.result{
+                    case .success(let element):do{
+                        
+                        
+                        let json = JSON(element)["results"]
+                        
+                        json.forEach{(_, json)in
+                            //                        print(json["name"].string ?? "エラー")
+                            self.nameArray.append(json["name"].string)
+                        }
+                        
+                    }
+                    case .failure:do{
+                        print("エラー2")
+                    }
+                    }
+                    
+                    
+                    
+                }
+            
+            
+        }
+        print(self.nameArray)
+        self.tableView.reloadData()
+        
+    }
 }
-
